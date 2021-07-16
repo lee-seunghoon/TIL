85eca945cc1213903516ef44ccf2b76bd5ecb083
sh0316 <sh0316@kakao.com>
2021-07-17 00:00:03 +0900

    ok

---
+++ Python_learning/기초개념/05-input/exam3.py
@@ -0,0 +1,6 @@
+# 키보드 입력값 실수로 바꾸기
+
+x=float(input("첫번째 실수를 입력하세요: "))
+y=float(input("두번째 실수를 입력하세요: "))
+print()
+print("%s + %s = %s" %(x, y, x+y))
---
+++ Python_learning/기초개념/05-input/exam4.py
@@ -0,0 +1,6 @@
+# True : 참 / False : 거짓
+
+name = input("이름을 입력하세요 : ")
+print(bool(name))
+print("이름 = %s" %(name))
+
