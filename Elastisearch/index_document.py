c9b0333cebd89af1e21e223cae636daa4e5b2ccd
lee-seunghoon <sh0316@kakao.com>
2023-02-20 13:55:19 +0900

    Add : 엘라스틱서치 document data indexing 모듈 추가

---
+++ Elastisearch/index_document.py
@@ -0,0 +1,26 @@
+"""
+데이터 indexing 기능 및 역할 담당 모듈
+"""
+
+import json
+from argparse import ArgumentParser
+from elasticsearch import Elasticsearch
+from elasticsearch.helpers import bulk
+
+
+def load_dataset(path):
+    with open(path) as f:
+        return [json.loads(line) for line in f]
+
+
+def main(args):
+    client = Elasticsearch('localhost:9200')
+    docs = load_dataset(args.data)
+    bulk(client, docs)
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(description='indexing ES documents.')
+    parser.add_argument('--data', help='ES documents.')
+    args = parser.parse_args()
+    main(args)
