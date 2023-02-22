961753e29dee9c44500988e26cf5bb1b0e8f504b
lee-seunghoon <sh0316@kakao.com>
2023-02-22 18:22:15 +0900

    Add : FastAPI 활용 MySQL DB 연동 기능 추가

---
+++ FastAPI+MySQL/database_connent.py
@@ -0,0 +1,23 @@
+import os, json
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_INFO_DIR = os.path.join(BASE_DIR, "db_info.json")
+db_info = json.loads(open(DB_INFO_DIR).read())
+DB = db_info["DB"]
+DB_URL = f"mysql+pymysql://{DB['user']}:{DB['password']}@{DB['host']}:{DB['port']}/{DB['database']}?charset=utf8"
+
+engine = create_engine(
+    DB_URL
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
